cc35295c1df71886c53390dfe0fcfe80f2cebff8
Danielle <hummakavula6105@gmail.com>
2022-06-18 12:22:24 -0400

    changed database settings + created cars app

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cars_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cars_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '@StarGazing13'
+
     }
 }
 
